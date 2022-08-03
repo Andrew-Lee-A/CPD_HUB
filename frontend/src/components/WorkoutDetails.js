4689158bbd1cac82cafe474b1409f253e26dd26c
Andrew-Lee-A <79880562+Andrew-Lee-A@users.noreply.github.com>
2022-08-03 20:22:27 +1200

    fix up

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,10 +1,28 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+
+//date fns
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
 const WorkoutDetails = ({ workout }) => {
+    const {dispatch} = useWorkoutsContext()
+    const handleClick = async () => {
+        const response = await fetch('/api/workouts/' + workout._id, {
+            method: 'DELETE'
+        })
+        const json = await response.json()
+        
+        if(response.ok){
+            dispatch({type: 'DELETE_WORKOUT', payload: json})
+        }
+    }
+    
     return (
         <div className="workout-details">
             <h4>{workout.title}</h4>
             <p><strong> Load (kg): </strong>{workout.load}</p>
             <p><strong> Reps : </strong>{workout.reps}</p>
-            <p>{workout.createdAt}</p>
+            <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+            <p><strong> Submitted: </strong>{formatDistanceToNow(new Date(workout.createdAt), {addSuffix: true})}</p>
         </div>
     )
 }
--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -8,6 +8,7 @@ const WorkoutForm = () => {
   const [load, setLoad] = useState('')
   const [reps, setReps] = useState('')
   const [error, setError] = useState(null)
+  const [emptyFields, setEmptyFields] = useState([])
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -25,12 +26,14 @@ const WorkoutForm = () => {
 
     if (!response.ok) {
       setError(json.error)
+      setEmptyFields(json.emptyFields)
     }
     if (response.ok) {
       setError(null)
       setTitle('')
       setLoad('')
       setReps('')
+      setEmptyFields([])
       dispatch({type: 'CREATE_WORKOUT', payload: json})
     }
 
@@ -45,6 +48,7 @@ const WorkoutForm = () => {
         type="text" 
         onChange={(e) => setTitle(e.target.value)} 
         value={title}
+        className={emptyFields.includes('title') ? 'error' : ''}
       />
 
       <label>Load (in kg):</label>
@@ -52,6 +56,7 @@ const WorkoutForm = () => {
         type="number" 
         onChange={(e) => setLoad(e.target.value)} 
         value={load}
+        className={emptyFields.includes('load') ? 'error' : ''}
       />
 
       <label>Number of Reps:</label>
@@ -59,6 +64,7 @@ const WorkoutForm = () => {
         type="number" 
         onChange={(e) => setReps(e.target.value)} 
         value={reps} 
+        className={emptyFields.includes('reps') ? 'error' : ''}
       />
 
       <button>Add Workout</button>
