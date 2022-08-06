0d2b96bf8e8718706275a87d34ed76fec184c3ac
Andrew-Lee-A <79880562+Andrew-Lee-A@users.noreply.github.com>
2022-08-07 04:51:27 +1200

    sign up hook done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 //pages and components
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -10,12 +12,13 @@ function App() {
       <BrowserRouter>
         <Navbar/>
         <div className="pages">
-        <Routes>
-            <Route 
-              path="/" 
-              element={<Home />} 
-            />
+
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/signup" element={<Signup/>} />
           </Routes>
+
         </div>
       </BrowserRouter>
     </div>
