9c63d7a07d1eef9ae0f924f57126fa781e956670
Andrew-Lee-A <79880562+Andrew-Lee-A@users.noreply.github.com>
2022-09-19 01:08:09 +1200

    progress bar update on backend fetch

--- frontend/src/components/ProgressBar.js
+++ frontend/src/components/ProgressBar.js
@@ -1,49 +1,75 @@
-import React from 'react'
-import ApexCharts from 'apexcharts'
+import { React, useEffect, useState } from "react";
+import ApexCharts from "apexcharts";
 
-import './progresbar.scss'
+import { useAuthContext } from "../hooks/useAuthContext";
 
+import "./progresbar.scss";
 
 const ProgressBar = () => {
-    var options = {
-        series: [44, 55, 67, 83],
-        chart: {
-        height: 350,
-        type: 'radialBar',
+  const [data, setData] = useState(null);
+  const { user } = useAuthContext();
+
+  const fetchCPDSummary = async () => {
+    const response = await fetch("/api/userDetails", {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
       },
-      plotOptions: {
-        radialBar: {
-          dataLabels: {
-            name: {
-              fontSize: '24px',
-            },
-            value: {
-              fontSize: '16px',
-            },
-            total: {
-              show: true,
-              label: 'Current CPD points',
-              formatter: function (w) {
-                // By default this function returns the average of all series. The below is just an example to show the use of custom formatter function
-                return 150
-              }
-            }
-          }
-        }
-      },
-      labels: ['Area of practice', 'Risk management', 'Business', 'Career Interests'],
-      };
+    });
+    const json = await response.json();
+    setData(json);
     
-      var chart = new ApexCharts(document.querySelector("#chart"), options);
-      chart.render();
-      console.log("rendering chart")
+    
+  };
+
+  useEffect(() => {
+    fetchCPDSummary();
+    const {areaOfPractice} = data
+  
+
+
+  var options = {
+    series: [areaOfPractice, 55, 67, 83],
+    chart: {
+      height: 350,
+      type: "radialBar",
+    },
+    plotOptions: {
+      radialBar: {
+        dataLabels: {
+          name: {
+            fontSize: "24px",
+          },
+          value: {
+            fontSize: "16px",
+          },
+          total: {
+            show: true,
+            label: "Current CPD points",
+            formatter: function (w) {
+              // By default this function returns the average of all series. The below is just an example to show the use of custom formatter function
+              return 150;
+            },
+          },
+        },
+      },
+    },
+    labels: [
+      "Area of practice",
+      "Risk management",
+      "Business",
+      "Career Interests",
+    ],
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  chart.render();
+}, [user]);
 
   return (
-    <div className='progressBar'>
+    <div className="progressBar">
       <div id="chart"></div>
     </div>
-  )
-}
+  );
+};
 
-export default ProgressBar
-
+export default ProgressBar;
