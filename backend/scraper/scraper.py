9c862e6196afa12fc2fb86aaa7899ebdf03aa78a
kmj4789 <81201559+kmj4789@users.noreply.github.com>
2022-10-14 03:24:15 +1300

    changing original scraper site back to engineering nz

--- backend/scraper/scraper.py
+++ backend/scraper/scraper.py
@@ -29,8 +29,8 @@ reg=[]
 
 ## provides the domain for the scraper to scrape in order to retrive it's content which is done using the BeautifulSoup's text-based html parser
 n_page = 17
-url_reg = "https://www.engineersaustralia.org.au/learning-and-events/events-webinars-and-courses"
-base_link = "https://www.engineersaustralia.org.au/learning-and-events/events-webinars-and-courses"
+url_reg = "https://www.engineeringnz.org/courses-events/"
+base_link = "https://www.engineeringnz.org/courses-events/?p="
 for i in range (1, n_page+1):
     url = base_link+str(i)
     #print(url)
