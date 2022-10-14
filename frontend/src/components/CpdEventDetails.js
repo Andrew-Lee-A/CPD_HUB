0a12f75a7ef65421d968a24fc4a8ff389f06cfc9
Andrew-Lee-A <79880562+Andrew-Lee-A@users.noreply.github.com>
2022-10-15 00:32:50 +1300

    [ref] temp fix to make backend compatible with ible with frontend

--- frontend/src/components/CpdEventDetails.js
+++ frontend/src/components/CpdEventDetails.js
@@ -7,7 +7,7 @@ import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 const CpdEventDetails = ({ cpdEvent }) => {
     const {dispatch} = useCpdEventsContext()
     const {user} = useAuthContext()
-    const date = new Date(cpdEvent.date.toString()).toLocaleString('en-NZ', {timezone: 'NZST'});
+    //const date = new Date(cpdEvent.date.toString()).toLocaleString('en-NZ', {timezone: 'NZST'});
 
     const handleClick = async () => {
         if(!user){
@@ -30,10 +30,10 @@ const CpdEventDetails = ({ cpdEvent }) => {
             <h4>{cpdEvent.title}</h4>
             <p><strong> CPD Points (hours): </strong>{cpdEvent.cpd_points}</p>
             <p><strong> Field : </strong>{cpdEvent.field}</p>
-            <p><strong> Date : </strong>{date}</p>
-            <p><strong> Location : </strong>{cpdEvent.location}</p>
+            <p><strong> Date : </strong>{cpdEvent.date}</p>
+            <p><strong> Price : </strong>{cpdEvent.price}</p>
+            <p><strong> Booking Url : </strong>{cpdEvent.booking_Url}</p>
             <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
-            <p><strong> Submitted: </strong>{formatDistanceToNow(new Date(cpdEvent.createdAt), {addSuffix: true})}</p>
         </div>
     )
 }
